64c767d28801ab1816876315d5cd3853d46306b9
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-01-07 12:19:43 -0600

    more efficient draft

--- triangular-loops.java
+++ triangular-loops.java
@@ -14,3 +14,20 @@ public class TriangularNumbers {
         }
     }
 }
+
+// Removes inner for loop by keeping track of triangular and adding i iteratively
+
+import java.util.*;
+
+public class CleverTriangularNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many triangular numbers? ");
+        int n = in.nextInt();
+        int triangular = 0;
+        for(int i = 1; i <= n; i++) {
+            triangular += i;
+            System.out.println(triangular);
+        }
+    }
+}
